16291a740cc5cfd6902aad09d2f06db8bdec80a2
airborne <airborne>
2002-11-02 08:10:57 +0000

    Added track number and length to track structure.  Added setter methods for track artist and title.

--- lib/cddb_track.c
+++ lib/cddb_track.c
@@ -1,7 +1,11 @@
 
 
 #include <stdlib.h>
+#include <string.h>
 #include "cddb/cddb_ni.h"
+
+
+/* --- construction / destruction */
 
 
 cddb_track_t *cddb_track_new(void)
@@ -9,7 +13,9 @@ cddb_track_t *cddb_track_new(void)
     cddb_track_t *track;
 
     track = (cddb_track_t*)calloc(1, sizeof(cddb_track_t));
+    track->num = -1;
     track->frame_offset = -1;
+    track->length = -1;
 
     return track;
 }
@@ -24,9 +30,35 @@ void cddb_track_destroy(cddb_track_t *track)
     }
 }
 
+
+/* --- getters & setters --- */
+
+
+void cddb_track_set_title(cddb_track_t *track, const char *title)
+{
+    if (track) {
+        FREE_NOT_NULL(track->title);
+        track->title = strdup(title);
+    }
+}
+
+void cddb_track_set_artist(cddb_track_t *track, const char *artist)
+{
+    if (track) {
+        FREE_NOT_NULL(track->artist);
+        track->artist = strdup(artist);
+    }
+}
+
+
+/* --- miscellaneous */
+
+
 void cddb_track_print(cddb_track_t *track)
 {
+    printf("    number: %d\n", track->num);
     printf("    frame offset: %d\n", track->frame_offset);
+    printf("    length: %d seconds\n", track->length);
     if (track->artist) printf("    artist: '%s'\n", track->artist);
     if (track->title) printf("    title: '%s'\n", track->title);
 }
