ee5682104fb3e4ac8a936352c8216e7713c38050
airborne <airborne>
2003-03-04 19:54:58 +0000

    minor debug logging updates

--- lib/cddb_net.c
+++ lib/cddb_net.c
@@ -9,7 +9,7 @@ char *sock_fgets(char *s, int size, int sock)
 {
     char *p = s;
 
-    dlog("sock_fgets");
+    dlog("sock_fgets()");
     size--;                      /* save one for terminating null */
     while (size) {
         /* read one byte */
@@ -40,7 +40,7 @@ size_t sock_fwrite(const void *ptr, size_t size, size_t nmemb, int sock)
     size_t total_size;
     int rv;
 
-    dlog("sock_fwrite");
+    dlog("sock_fwrite()");
     total_size = size * nmemb;
     rv = send(sock, (const char *)ptr, total_size, 0);
     return rv/size;
@@ -51,7 +51,7 @@ int sock_fprintf(int sock, const char *format, ...)
     int rv;
     va_list args;
 
-    dlog("sock_fprintf");
+    dlog("sock_fprintf()");
     va_start(args, format);
     rv = sock_vfprintf(sock, format, args);
     va_end(args);
@@ -63,7 +63,7 @@ int sock_vfprintf(int sock, const char *format, va_list ap)
     char buf[1024];
     int rv;
    
-    dlog("sock_vfprintf");
+    dlog("sock_vfprintf()");
     rv = vsnprintf(buf, sizeof(buf), format, ap);
     dlog("\tbuf = '%s'", buf);
     if (rv >= sizeof(buf)) {
