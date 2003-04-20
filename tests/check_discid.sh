9869695a6c6076067d019f442f45715227d67a55
airborne <airborne>
2003-04-20 22:23:44 +0000

    fixed spelling typo

--- tests/check_discid.sh
+++ tests/check_discid.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Id: check_discid.sh,v 1.1 2003/04/20 22:22:27 airborne Exp $
+# $Id: check_discid.sh,v 1.2 2003/04/20 22:23:44 airborne Exp $
 
 . settings.sh
 
@@ -10,7 +10,7 @@
 $CDDB_QUERY calc 0 0 | check_discid '00000000'
 
 #
-# Check disc ID calculation of an real disc
+# Check disc ID calculation of real disc
 #
 $CDDB_QUERY calc 3826 11 150 28615 51027 75835 102620 121460 148977 \
                  175697 204322 231082 268002 | check_discid '920ef00b'
