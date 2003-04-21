1397f9fb3999e89a1f62b2820e0b0383be6cb978
airborne <airborne>
2003-04-21 11:04:56 +0000

    added some disc title and artist parsing tests

--- tests/check_parse.sh
+++ tests/check_parse.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 #
-# $Id: check_parse.sh,v 1.1 2003/04/21 10:20:33 airborne Exp $
+# $Id: check_parse.sh,v 1.2 2003/04/21 11:04:56 airborne Exp $
 
 . settings.sh
 
-#
-# 
-#
-$CDDB_QUERY -c only -D $CDDB_CACHE read misc 12345678 | check_read '12345678'
+# Test parsing of some locally cached entries.  These entries are
+# designed to test the parsing of all supported fields.  Mutli-line
+# fields are also tested in every possible way.
+
+for id in 12345678 12345679 1234567a 1234567b 1234567c 1234567d \
+          1234567e 1234567f 12345680 12345681 12345682 12345683 ; do
+    $CDDB_QUERY -c only -D $CDDB_CACHE read misc $id | check_read $id
+done
 
 #
 # Fell through all tests
