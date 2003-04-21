725b3d9fbf16ddd96b86e116d44149df70aaeebd
airborne <airborne>
2003-04-21 12:07:46 +0000

    Initial revision

---
+++ tests/check_cache.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# $Id: check_cache.sh,v 1.1 2003/04/21 12:09:20 airborne Exp $
+
+. settings.sh
+
+# This script implements a set of tests to check whether the local
+# CDDB cache functionality is working correctly.
+
+# under construction
+exit $SKIPPED
+
+#
+# Fell through all tests
+#
+exit $SUCCESS
---
+++ tests/check_server.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# $Id: check_server.sh,v 1.1 2003/04/21 12:07:46 airborne Exp $
+
+. settings.sh
+
+# This script implements a set of tests to check whether the CDDB
+# server access functionality is working correctly.
+
+# under construction
+exit $SKIPPED
+
+#
+# Fell through all tests
+#
+exit $SUCCESS
