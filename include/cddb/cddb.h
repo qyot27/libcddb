f261408832ca5d16c6d309f7a1152756099ab9d7
airborne <airborne>
2003-05-01 08:12:43 +0000

    moved some doc from doxygen to webpage

--- include/cddb/cddb.h
+++ include/cddb/cddb.h
@@ -1,5 +1,5 @@
 /*
-    $Id: cddb.h,v 1.5 2003/04/14 22:25:49 airborne Exp $
+    $Id: cddb.h,v 1.6 2003/05/01 08:12:43 airborne Exp $
 
     Copyright (C) 2003 Kris Verbeeck <airborne@advalvas.be>
 
@@ -35,26 +35,6 @@
 
 /**
  * \mainpage libCDDB, a C API for CDDB server access
- *
- * This page is under construction.
- *
- * \htmlonly
- *   The project homepage can be found
- *   <a href="http://sourceforge.net/projects/libcddb/">here</a>.
- *
- *   <p>
- *
- *   If you have any questions, problems or whatever with the usage of
- *   this library then just use the appropriate SourceForge facilities
- *   on the project homepage to report them.
- *
- *   <p>
- *
- *   <a href="http://sourceforge.net/">
- *     <img src="http://sourceforge.net/sflogo.php?group_id=65237&amp;type=5" 
- *          width="105" height="31" border="0" alt="SourceForge Logo">
- *   </a> 
- * \endhtmlonly
  */
 
 
