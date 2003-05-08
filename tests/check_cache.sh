d0dbc699c981f3a5655da80153d037096e6a8e59
airborne <airborne>
2003-05-08 20:40:06 +0000

    added tests to check changing of cache dir

--- tests/check_cache.sh
+++ tests/check_cache.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Id: check_cache.sh,v 1.5 2003/05/08 19:40:40 airborne Exp $
+# $Id: check_cache.sh,v 1.6 2003/05/08 20:40:06 airborne Exp $
 
 . ./settings.sh
 
@@ -15,34 +15,44 @@ mkdir $CACHE
 #
 # Read from cache
 #
+DISCID0='12345678'
 DISCID1='920ef00b'
 DISCID2='920ef00c'
 
-# 1. read from cache only (should fail because cache is empty)
+# check setting of cache dir (-D option)
+cddb_query -c only -D $CDDB_CACHE read misc $DISCID0
+check_read $? $DISCID0
+
+# check cache dir tilde expansion
+HOME=$CDDB_CACHE
+cddb_query -c only -D '~' read misc $DISCID0
+check_read $? $DISCID0
+
+# read from cache only (should fail because cache is empty)
 cddb_query -c only -D $CACHE read misc $DISCID1
 check_not_found $? $DISCID1
 
-# 2. read from server and disable cache
+# read from server and disable cache
 cddb_query -c off -D $CACHE read misc $DISCID1
 check_read $? $DISCID1
 
-# 3. read from cache only (should still fail because previous test did not cache)
+# read from cache only (should still fail because previous test did not cache)
 cddb_query -c only -D $CACHE read misc $DISCID1
 check_not_found $? $DISCID1
 
-# 4. read from server and enable cache
+# read from server and enable cache
 cddb_query -c on -D $CACHE read misc $DISCID1
 check_read $? $DISCID1
 
-# 5. read from cache only (should succeed because previous test filled cache)
+# read from cache only (should succeed because previous test filled cache)
 cddb_query -c only -D $CACHE read misc $DISCID1
 check_read $? $DISCID1
 
-# 6. enable cache and try to fetch non-existing disc (should fail)
+# enable cache and try to fetch non-existing disc (should fail)
 cddb_query -c on -D $CACHE read misc $DISCID2
 check_not_found $? $DISCID2
 
-# 7. create non-existing disc in cache and read again (should succeed now)
+# create non-existing disc in cache and read again (should succeed now)
 cp $CACHE/misc/$DISCID1 $CACHE/misc/$DISCID2
 cddb_query -c on -D $CACHE read misc $DISCID2
 check_read $? $DISCID2
