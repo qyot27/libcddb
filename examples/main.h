dc09bdf7bab190163e83992f9928d00b8e38533d
airborne <airborne>
2005-05-29 08:21:43 +0000

    sites command integration

--- examples/main.h
+++ examples/main.h
@@ -1,5 +1,5 @@
 /*
-    $Id: main.h,v 1.11 2005/03/11 21:29:27 airborne Exp $
+    $Id: main.h,v 1.12 2005/05/29 08:21:43 airborne Exp $
 
     Copyright (C) 2003, 2004, 2005 Kris Verbeeck <airborne@advalvas.be>
 
@@ -48,3 +48,5 @@ void do_display(cddb_disc_t *disc);
 cddb_disc_t *cd_read(char *device);
 
 cddb_disc_t *cd_create(int dlength, int tcount, int *foffset, int use_time);
+
+void do_sites(cddb_conn_t *conn);
