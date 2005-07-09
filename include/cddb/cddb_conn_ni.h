723481f006fc66a4e0aef65768afdbcf64b17a79
airborne <airborne>
2005-07-09 08:34:56 +0000

    moved actual defintion of connection structure to this file

--- include/cddb/cddb_conn_ni.h
+++ include/cddb/cddb_conn_ni.h
@@ -1,5 +1,5 @@
 /*
-    $Id: cddb_conn_ni.h,v 1.11 2005/03/11 21:29:29 airborne Exp $
+    $Id: cddb_conn_ni.h,v 1.12 2005/07/09 08:34:56 airborne Exp $
 
     Copyright (C) 2003, 2004, 2005 Kris Verbeeck <airborne@advalvas.be>
 
@@ -26,14 +26,85 @@
     extern "C" {
 #endif
 
+
 #include "cddb_ni.h"
+#include "ll.h"
 
 
-struct cddb_iconv_s {
+/* --- type definitions */
+
+
+/** Actual definition of iconv structure. */
+struct cddb_iconv_s
+{
     iconv_t cd_to_freedb;       /**< character set conversion descriptor for
                                      converting from user to FreeDB format */
     iconv_t cd_from_freedb;     /**< character set conversion descriptor for
                                      converting from FreeDB to user format */
+};
+
+/** Actual definition of connection structure. */
+struct cddb_conn_s 
+{
+    unsigned int buf_size;      /**< maximum line/buffer size, defaults to 1024
+                                     (see DEFAULT_BUF_SIZE) */
+    char *line;                 /**< last line read */
+
+    int is_connected;           /**< are we already connected to the server? */
+    struct sockaddr_in sa;      /**< the socket address structure for
+                                     connecting to the CDDB server */
+    int socket;                 /**< the socket file descriptor */
+    char *server_name;          /**< host name of the CDDB server, defaults
+                                     to 'freedb.org' (see DEFAULT_SERVER) */
+    int server_port;            /**< port of the CDDB server, defaults to 888 
+                                     (see DEFAULT_PORT) */
+    int timeout;                /**< time out interval (in seconds) used during
+                                     network operations, defaults to 10 seconds
+                                     (see DEFAULT_TIMEOUT) */
+
+    char *http_path_query;      /**< URL for querying the server through HTTP,
+                                     defaults to /~cddb/cddb.cgi'
+                                     (see DEFAULT_PATH_QUERY) */
+    char *http_path_submit;     /**< URL for submitting to the server through HTTP,
+                                     defaults to /~cddb/submit.cgi'
+                                     (see DEFAULT_PATH_SUBMIT) */
+    int is_http_enabled;        /**< use HTTP, disabled by default */
+
+    int is_http_proxy_enabled;  /**< use HTTP through a proxy server,
+                                     disabled by default */
+    char *http_proxy_server;    /**< host name of the HTTP proxy server */
+    int http_proxy_server_port; /**< port of the HTTP proxy server,
+                                     defaults to 8080 (see DEFAULT_PROXY_PORT) */
+    char *http_proxy_username;  /**< HTTP proxy user name */
+    char *http_proxy_password;  /**< HTTP proxy password */
+    char *http_proxy_auth;      /**< Base64 encoded username:password */
+
+    FILE *cache_fp;             /**< a file pointer to a cached CDDB entry or
+                                     NULL if no cached version is available */
+    cddb_cache_mode_t use_cache;/**< field to specify local CDDB cache behaviour, 
+                                     enabled by default (CACHE_ON) */
+    char *cache_dir;            /**< CDDB slave cache, defaults to 
+                                     '~/.cddbslave' (see DEFAULT_CACHE) */
+    int cache_read;             /**< read data from cached file instead of
+                                     from the network */
+
+    char *cname;                /**< name of the client program, 'libcddb' by
+                                     default */
+    char *cversion;             /**< version of the client program, current 
+                                     libcddb version by default */
+    char *user;                 /**< user name supplied to CDDB server, defaults
+                                     to the value of the 'USER' environment 
+                                     variable or 'anonymous' if undefined */
+    char *hostname;             /**< host name of the local machine, defaults
+                                     to the value of the 'HOSTNAME' environment
+                                     variable or 'localhost' if undefined */
+
+    cddb_error_t errnum;        /**< error number of last CDDB command */
+
+    list_t *query_data;         /**< list to keep CDDB query results */
+    list_t *sites_data;         /**< list to keep FreeDB mirror sites */
+
+    cddb_iconv_t charset;       /**< character set conversion settings */
 };
 
 
